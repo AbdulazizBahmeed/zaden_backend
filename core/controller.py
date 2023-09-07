be5059eb5cab891a5dd387263be599d23e8ade2c
abulaziz bahamid <gimo999@hotmail.com>
2023-09-07 20:23:55 +0300

    fixed the bug in the regex for validating credentials

--- core/controller.py
+++ core/controller.py
@@ -3,11 +3,25 @@ from django.http import JsonResponse
 from django.contrib.auth import get_user_model
 import re
 
+def validaite_email(email):
+    pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$"
+    if email is not None and re.match(pattern,email):
+        return True
+    else:
+        return False
+
+def validaite_passowrd(password):
+    pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,}$"
+    if password is not None and re.match(pattern,password):
+        return True
+    else:
+        return False
 
 def signup(req):
     if req.method=="POST":
         email =email=req.POST.get("email")
         password =req.POST.get("password")
+
         if validaite_email(email) and validaite_passowrd(password):
             UserModel = get_user_model()
             user = UserModel(email=email)
@@ -28,7 +42,7 @@ def signup(req):
             return JsonResponse(
                 {
                     "status":False,
-                    "message":"wrong email or password foramt",
+                    "message":"wrong email or password format",
                 },status =400
             )
     else:
@@ -46,7 +60,8 @@ def login(req):
             login_user(req,user)
             return JsonResponse({
                 "status":True,
-                "message":"User Logged In Succefully"
+                "message":"User Logged In Succefully",
+                "user": user.email
             }
             )
         else:
@@ -71,17 +86,4 @@ def logout(req):
             "status":False,
             "message":"wrong method"
         },status=405)
-
-def validaite_email(email):
-    pattern = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,3}"
-    if email is not None and re.match(email,pattern):
-        return True
-    else:
-        return False
-
-def validaite_passowrd(password):
-    pattern = "[a-zA-Z0-9]{6,}"
-    if password is not None and re.match(password,pattern):
-        return True
-    else:
-        return False
+    
