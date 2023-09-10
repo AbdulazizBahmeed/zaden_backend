fade9ee4b030513711b5682fec14fc6a179b2ca2
AbdulazizBahmeed <abdulazizbahmeed@outlook.sa>
2023-09-10 20:41:22 +0300

    added the login via google api and localized the responses to the front end

--- core/controller.py
+++ core/controller.py
@@ -7,17 +7,19 @@ import json
 
 def validaite_email(email):
     pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}$"
-    if email is not None and re.match(pattern,email):
+    if email is not None and re.match(pattern, email):
         return True
     else:
         return False
 
+
 def validaite_passowrd(password):
     pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,}$"
-    if password is not None and re.match(pattern,password):
+    if password is not None and re.match(pattern, password):
         return True
     else:
         return False
+
 
 def signup(req):
     if req.method=="POST":
@@ -27,36 +29,37 @@ def signup(req):
         fullname = json_data.get("fullname")
 
         if validaite_email(email) and validaite_passowrd(password):
-            UserModel = get_user_model()
-            user = UserModel(email=email,fullname=fullname)
+            user_model = get_user_model()
+            user = user_model(email=email, fullname=fullname)
             user.set_password(password)
             try:
                 user.save()
             except IntegrityError:
                 return JsonResponse({
-                    "status":False,
-                    "message":"User credentials already used"
-                },status=400)
-            login_user(req,user)
+                    "status": False,
+                    "message": "يوجد حساب مسجل بهذا البريد الإلكتروني"
+                }, status=400)
+            login_user(req, user)
             return JsonResponse({
-                "status":True,
-                "message":"user signed up succefully",
+                "status": True,
+                "message": "تم التسجيل بنجاح",
                 "fullname": user.fullname,
                 "email": user.email,
                 "user_id": user.id
-                })
+            })
         else:
             return JsonResponse(
                 {
-                    "status":False,
-                    "message":"wrong email or password format",
-                },status =400
+                    "status": False,
+                    "message": "خطأ في صيغة الايميل او كلمة السر",
+                }, status=400
             )
     else:
         return JsonResponse({
-            "status":False,
-            "message":"wrong method"
-        },status=405)
+            "status": False,
+            "message": "wrong method"
+        }, status=405)
+
 
 def login(req):
     if req.method=="POST":
@@ -65,10 +68,10 @@ def login(req):
         password =json_data.get("password")
         user = authenticate(email=email,password=password)
         if user is not None:
-            login_user(req,user)
+            login_user(req, user)
             return JsonResponse({
-                "status":True,
-                "message":"User Logged In Succefully",
+                "status": True,
+                "message": "تم تسجيل الدخول بنجاح",
                 "fullname": user.fullname,
                 "email": user.email,
                 "user_id": user.id
@@ -76,24 +79,51 @@ def login(req):
             )
         else:
             return JsonResponse({
-            "status":False,
-            "message":"there is no user with that credentials"
-        },status=400)
+                "status": False,
+                "message": "لايوجد مستخدم بهذا البريد الإلكتروني وكلمة السر-"
+            }, status=400)
     else:
         return JsonResponse({
-            "status":False,
-            "message":"wrong method"
-        },status=405)
+            "status": False,
+            "message": "wrong method"
+        }, status=405)
+
+
+def google_login(req):
+    if req.method == "POST":
+        json_data = json.loads(req.body)
+        email = json_data.get("email")
+        fullname = json_data.get("fullname")
+        user_model = get_user_model()
+        user = user_model(email=email, fullname=fullname)
+        try:
+            user.save()
+        except IntegrityError:
+            user = user_model.objects.get(email=email)
+        login_user(req, user)
+        return JsonResponse({
+            "status": True,
+            "message": "تم تسجيل الدخول بنجاح",
+            "fullname": user.fullname,
+            "email": user.email,
+            "user_id": user.id
+        })
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "wrong method"
+        }, status=405)
+
 
 def logout(req):
-    if req.method=="GET":
+    if req.method == "GET":
         logout_user(req)
         return JsonResponse({
-            "message":"logged out succefully"
+            "message": "تم تسجيل الخروج بنجاح"
         })
     else:
         return JsonResponse({
-            "status":False,
-            "message":"wrong method"
-        },status=405)
-    
+            "status": False,
+            "message": "wrong method"
+        }, status=405)
