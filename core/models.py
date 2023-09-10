b7ea458def934d9cdcf7a0f385d04195e72ea3f9
abdulaziz bahmeed <62620250+AbdulazizBahmeed@users.noreply.github.com>
2023-09-10 21:34:38 +0300

    Google login edit (#14)
    added th picture columnin data base and edited the returned it in the api

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     first_name = None
     last_name = None
     fullname = models.CharField(max_length=255, null=True,blank=True)
-    email = models.EmailField("email", unique=True)
+    email = models.EmailField(unique=True)
+    picture = models.TextField(blank=True, null=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
