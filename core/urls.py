e6956930fd5fe48a8bb96df63e742bb94e9b1bb3
abdulaziz bahmeed <62620250+AbdulazizBahmeed@users.noreply.github.com>
2023-09-10 20:43:44 +0300

    added the login via google api and localized the responses to the front end (#13)

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from . import controller
 urlpatterns = [
     path('signup/',controller.signup),
     path('login/',controller.login),
+    path('google-login/',controller.google_login),
     path('logout/',controller.logout),
 ]
