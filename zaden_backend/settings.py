4d38c90d0886d852238f8cb755d1b60e82a08e48
abdulaziz bahmeed <62620250+AbdulazizBahmeed@users.noreply.github.com>
2023-09-16 17:46:27 +0300

    Add forecast (#16)
    changed the secret key

--- zaden_backend/settings.py
+++ zaden_backend/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from django.core.management.utils import get_random_secret_key
+# from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get('SECRET_KEY', 'True')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') != 'False'
