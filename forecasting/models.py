f602cbb51d3af2261e842845fce31e54121f5358
abdulaziz bahmeed <62620250+AbdulazizBahmeed@users.noreply.github.com>
2023-09-28 16:24:17 +0300

    File name (#25)
    added file_name in files model

--- forecasting/models.py
+++ forecasting/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.conf import settings
 
 class File(models.Model):
+    file_name = models.CharField(max_length=255)
     file = models.FileField(upload_to="")
     owner = models.ForeignKey(get_user_model(), related_name="files",on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,7 +12,7 @@ class File(models.Model):
     def as_dict(self):
         return {
             "id": self.id,
-            "name": self.file.name,
+            "name": self.file_name,
             "date" : self.created_at.astimezone(timezone.get_current_timezone()).strftime("%Y-%m-%d %I:%M%p"),
             # "date": localtime(self.created_at).strftime("%Y-%m-%d %I:%M %p")
         }
