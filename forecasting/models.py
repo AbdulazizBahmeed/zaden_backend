d3d712a5149b75ff1d9b78430cf1c9c4d054fe44
AbdulazizBahmeed <abdulazizbahmeed@outlook.sa>
2023-10-04 22:21:37 +0300

    made the uploadning happens in the AWS s3 bucket sotrage

--- forecasting/models.py
+++ forecasting/models.py
@@ -1,11 +1,13 @@
+from io import BytesIO
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from django.conf import settings
+import requests
+
 
 class File(models.Model):
     file_name = models.CharField(max_length=255)
-    file = models.FileField(upload_to="")
+    uuid = models.UUIDField()
     owner = models.ForeignKey(get_user_model(), related_name="files",on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -16,3 +18,9 @@ class File(models.Model):
             "date" : self.created_at.astimezone(timezone.get_current_timezone()).strftime("%Y-%m-%d %I:%M%p"),
             # "date": localtime(self.created_at).strftime("%Y-%m-%d %I:%M %p")
         }
+    
+    def file(self):
+        url = f'https://kpnzs85sk8.execute-api.ap-northeast-2.amazonaws.com/download-api/zaden-bucket/{self.uuid}.xlsx'
+        response = requests.get(url)
+        file = BytesIO(response.content)
+        return file
